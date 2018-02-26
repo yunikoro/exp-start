5331c6db37c81d2b0a6bf620a6519d2b4e7f32ad
黄知恒 <huangzhiheng@xiaojubianli.com>
2018-02-26 10:52:16 +0800

    bugfix

--- routes/wxAuth.js
+++ routes/wxAuth.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-router.post('/auth', function(req, res) {
+/*router.post('/auth', function(req, res) {
     console.info('test log');
     console.warn('this is a warning');
     res.send('hello world');
-});
+});*/
 
 router.all('/getcoupon', function (req, res) {
     logger.debug('in getcoupon');
