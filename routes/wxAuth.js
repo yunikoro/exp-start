4aa83d1629e0023fe47b1bd7e3d69f353c7eeed2
黄知恒 <huangzhiheng@xiaojubianli.com>
2018-02-26 15:18:16 +0800

    couponlist api update

--- routes/wxAuth.js
+++ routes/wxAuth.js
@@ -3,6 +3,50 @@ var router = express.Router();
 var log4js = require('log4js');
 var logger = log4js.getLogger();
 
+const couponList = [
+    {
+        id: 10,
+        coupon_type: 3,
+        sku_alias: '',
+        manzeng_good_id: null,
+        coupon_amount: 10.00,
+        coupon_base_amount: 30.00,
+        user_id: 5095,
+        valid_end_time: 1530415653
+    },
+    {
+        id: 11,
+        coupon_type: 4,
+        sku_alias: '',
+        manzeng_good_id: null,
+        coupon_amount: 0.5,
+        coupon_base_amount: 30.00,
+        user_id: 5095,
+        valid_end_time: 1530615653
+    },
+    {
+        id: 12,
+        coupon_type: 2,
+        sku_alias: '',
+        manzeng_good_id: null,
+        coupon_amount: 10.00,
+        coupon_base_amount: 30.00,
+        user_id: 5095,
+        valid_end_time: 1534495693
+    },
+    {
+        id: 13,
+        coupon_type: 6,
+        sku_alias: '小黄鸭',
+        manzeng_good_id: 52,
+        coupon_amount: 0.5,
+        coupon_base_amount: -1,
+        user_id: 5095,
+        valid_end_time: 1530492653
+    },
+];
+
+
 router.post('/auth', function(req, res) {
     console.info('test log');
     console.warn('this is a warning');
@@ -12,7 +56,7 @@ router.post('/auth', function(req, res) {
 router.all('/getcoupon', function (req, res) {
     logger.debug('in getcoupon');
     res.status(200)
-       .json({test: 'test'});
+       .json(couponList);
 })
 
 module.exports = router;
