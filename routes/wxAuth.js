119461b82425d547ae358915f70e5c0482eb1fda
黄知恒 <huangzhiheng@xiaojubianli.com>
2018-02-26 11:02:15 +0800

    open auth

--- routes/wxAuth.js
+++ routes/wxAuth.js
@@ -3,16 +3,16 @@ var router = express.Router();
 var log4js = require('log4js');
 var logger = log4js.getLogger();
 
-/*router.post('/auth', function(req, res) {
+router.post('/auth', function(req, res) {
     console.info('test log');
     console.warn('this is a warning');
     res.send('hello world');
-});*/
+});
 
 router.all('/getcoupon', function (req, res) {
     logger.debug('in getcoupon');
     res.status(200)
-       .jsonp({test: 'test'});
+       .json({test: 'test'});
 })
 
 module.exports = router;
