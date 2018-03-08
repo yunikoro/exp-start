83447283cf1e3ebbe366cdf48b02a3d400fcc630
黄知恒 <huangzhiheng@xiaojubianli.com>
2018-03-08 12:45:46 +0800

    商品接口 mooc

--- routes/wxAuth.js
+++ routes/wxAuth.js
@@ -143,6 +143,251 @@ var couponList = [
     }
 ];
 
+var goodsList = [
+    {
+        id: -1,
+        list: [
+            {
+                content: "钙芝威化饼干",
+                id: 2,
+                idDiscount: true,
+                img: "https://mgo.18jian.cn/uploads/2017-12-20/2017125a3a2936b0843.png",
+                marketPrice: "2.30",
+                name: "钙芝威化饼干",
+                num: 2,
+                price: "2.00"
+            },
+            {
+                content: "维他奶+LIPO面包干",
+                id: 3,
+                idDiscount: true,
+                img: "https://mgo.18jian.cn/uploads/2017-12-20/2017125a3a29fbba948.png",
+                marketPrice: "6.20",
+                name: "维他奶+LIPO面包干",
+                num: 4,
+                price: "5.80"
+            },
+            {
+                content: "半价爆款",
+                id: 4,
+                idDiscount: true,
+                img: "https://mgo.18jian.cn//uploads/2017-11-16/2017115a0ceea3f056e.png",
+                marketPrice: "5.00",
+                name: "红牛·战马饮料/瓶",
+                num: 1,
+                price: "2.50"
+            },
+            {
+                content: "喝了和驴一样狠",
+                id: 5,
+                idDiscount: false,
+                img: "https://mgo.18jian.cn//uploads/2017-11-16/2017115a0ceea3f056e.png",
+                marketPrice: "5.00",
+                name: "蓝驴·骡子饮料/瓶",
+                num: 1,
+                price: "5.00"
+            },
+            {
+                content: "最好喝的水",
+                id: 6,
+                isDiscount: false,
+                img: "https://mgo.18jian.cn//uploads/2017-11-16/2017115a0ceea3f056e.png",
+                marketPrice: "2.50",
+                name: "凉白开饮料/瓶",
+                num: 4,
+                price: "2.50"
+            },
+            {
+                content: "提升醒脑，尊贵享受",
+                id: 7,
+                isDiscount: false,
+                img: "https://mgo.18jian.cn//uploads/2017-11-16/2017115a0ceea3f056e.png",
+                marketPrice: "4.00",
+                name: "雀巢咖啡饮料/瓶",
+                num: 1,
+                price: "4.00"
+            },
+            {
+                content: "脆脆脆",
+                id: 8,
+                isDiscount: false,
+                img: "https://mgo.18jian.cn//uploads/2017-11-16/2017115a0ceea3f056e.png",
+                marketPrice: "2.20",
+                name: "汪汪雪饼",
+                num: 1,
+                price: "2.20"
+            },
+            {
+                content: "贪多嚼不烂",
+                id: 9,
+                isDiscount: false,
+                img: "https://mgo.18jian.cn//uploads/2017-11-16/2017115a0ceea3f056e.png",
+                marketPrice: "8.30",
+                name: "爷爷牛肉干",
+                num: 1,
+                price: "8.30"
+            },
+            {
+                content: "没有黄曲霉",
+                id: 10,
+                isDiscount: true,
+                img: "https://mgo.18jian.cn//uploads/2017-11-16/2017115a0ceea3f056e.png",
+                marketPrice: "12.70",
+                name: "咬升级花生",
+                num: 1,
+                price: "8.50"
+            }
+
+        ],
+        name: "全部",
+        sort: -1,
+        status: false
+    },
+    {
+        id: 0,
+        list: [
+            {
+                content: "钙芝威化饼干",
+                id: 2,
+                isDiscount: true,
+                img: "https://mgo.18jian.cn/uploads/2017-12-20/2017125a3a2936b0843.png",
+                marketPrice: "2.30",
+                name: "钙芝威化饼干",
+                num: 2,
+                price: "2.00"
+            },
+            {
+                content: "维他奶+LIPO面包干",
+                id: 3,
+                isDiscount: true,
+                img: "https://mgo.18jian.cn/uploads/2017-12-20/2017125a3a29fbba948.png",
+                marketPrice: "6.20",
+                name: "维他奶+LIPO面包干",
+                num: 4,
+                price: "5.80"
+            },
+            {
+                content: "半价爆款",
+                id: 4,
+                isDiscount: true,
+                img: "https://mgo.18jian.cn//uploads/2017-11-16/2017115a0ceea3f056e.png",
+                marketPrice: "5.00",
+                name: "红牛·战马饮料/瓶",
+                num: 1,
+                price: "2.50"
+            }
+        ],
+        name: "优惠",
+        sort: 0,
+        status: false
+    },
+    {
+        id: 1,
+        list: [
+            {
+                content: "脆脆脆",
+                id: 8,
+                isDiscount: false,
+                img: "https://mgo.18jian.cn//uploads/2017-11-16/2017115a0ceea3f056e.png",
+                marketPrice: "2.20",
+                name: "汪汪雪饼",
+                num: 1,
+                price: "2.20"
+            },
+            {
+                content: "贪多嚼不烂",
+                id: 9,
+                isDiscount: false,
+                img: "https://mgo.18jian.cn//uploads/2017-11-16/2017115a0ceea3f056e.png",
+                marketPrice: "8.30",
+                name: "爷爷牛肉干",
+                num: 1,
+                price: "8.30"
+            },
+            {
+                content: "没有黄曲霉",
+                id: 10,
+                isDiscount: true,
+                img: "https://mgo.18jian.cn//uploads/2017-11-16/2017115a0ceea3f056e.png",
+                marketPrice: "12.70",
+                name: "咬升级花生",
+                num: 1,
+                price: "8.50"
+            },
+            {
+                content: "钙芝威化饼干",
+                id: 2,
+                idDiscount: true,
+                img: "https://mgo.18jian.cn/uploads/2017-12-20/2017125a3a2936b0843.png",
+                marketPrice: "2.30",
+                name: "钙芝威化饼干",
+                num: 2,
+                price: "2.00"
+            },
+            {
+                content: "维他奶+LIPO面包干",
+                id: 3,
+                idDiscount: true,
+                img: "https://mgo.18jian.cn/uploads/2017-12-20/2017125a3a29fbba948.png",
+                marketPrice: "6.20",
+                name: "维他奶+LIPO面包干",
+                num: 4,
+                price: "5.80"
+            }
+        ],
+        name: "零食",
+        sort: 1,
+        status: false
+    },
+    {
+        id: 2,
+        list: [
+            {
+                content: "半价爆款",
+                id: 4,
+                idDiscount: true,
+                img: "https://mgo.18jian.cn//uploads/2017-11-16/2017115a0ceea3f056e.png",
+                marketPrice: "5.00",
+                name: "红牛·战马饮料/瓶",
+                num: 1,
+                price: "2.50"
+            },
+            {
+                content: "喝了和驴一样狠",
+                id: 5,
+                idDiscount: false,
+                img: "https://mgo.18jian.cn//uploads/2017-11-16/2017115a0ceea3f056e.png",
+                marketPrice: "5.00",
+                name: "蓝驴·骡子饮料/瓶",
+                num: 1,
+                price: "5.00"
+            },
+            {
+                content: "最好喝的水",
+                id: 6,
+                isDiscount: false,
+                img: "https://mgo.18jian.cn//uploads/2017-11-16/2017115a0ceea3f056e.png",
+                marketPrice: "2.50",
+                name: "凉白开饮料/瓶",
+                num: 4,
+                price: "2.50"
+            },
+            {
+                content: "提升醒脑，尊贵享受",
+                id: 7,
+                isDiscount: false,
+                img: "https://mgo.18jian.cn//uploads/2017-11-16/2017115a0ceea3f056e.png",
+                marketPrice: "4.00",
+                name: "雀巢咖啡饮料/瓶",
+                num: 1,
+                price: "4.00"
+            }
+        ],
+        name: "饮料",
+        sort: 2,
+        status: false
+    }
+]
 
 router.post('/auth', function(req, res) {
     console.info('test log');
@@ -159,5 +404,17 @@ router.all('/getcoupon', function (req, res) {
            data: couponList
        });
 })
+
+router.all('/getgoods', function (req, res) {
+    logger.debug('in getcoupon');
+    res.status(200)
+        .json({
+            err_msg: 'success',
+            error_code: 0,
+            data: goodsList
+        });
+})
+
+// goodsList
 
 module.exports = router;
