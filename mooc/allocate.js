687a5c78c6aa11a906ca5241a71a69339be59752
黄知恒 <huangzhiheng@xiaojubianli.com>
2018-03-23 16:36:49 +0800

    新的接口

--- mooc/allocate.js
+++ mooc/allocate.js
@@ -1,292 +1,136 @@
-const alllocateList = [
+
+const allocateDTOList = [
     {
-        boxAllocateSn: '3213213123123123',
-        devliverName: '李鬼',
-        outTime: '2018-03-19 10:59:44',
-        stationName: '北斗门换电站',
+        boxAllocateSn: '00020022018031948968',
+        deliveryName: '王五',
+        totalBox: 55,
         status: 1,
-        totalBox: 444
+        stationName: '福地元站',
+        outTime: 1515254400000,
     },
     {
-        boxAllocateSn: '20180313213526542',
-        devliverName: '宋江',
-        outTime: '2018-03-19 10:59:44',
-        stationName: '南斗门换电站',
+        boxAllocateSn: '12asasdas3',
+        deliveryName: '吴彦祖',
+        totalBox: 121,
         status: 1,
-        totalBox: 523
+        stationName: '凤起路站',
+        outTime: null,
     },
     {
-        boxAllocateSn: '2018031321326542',
-        devliverName: '张xiao chuan',
-        outTime: '2018-03-19 10:59:44',
-        stationName: '西斗门换电站',
+        boxAllocateSn: '341112',
+        deliveryName: '李白',
+        totalBox: 234,
         status: 1,
-        totalBox: 227
+        stationName: '西溪站',
+        outTime: 1521428384000,
     },
     {
-        boxAllocateSn: '2018031532131232',
-        devliverName: '',
-        outTime: '2018-03-19 10:59:44',
-        stationName: '东斗门换电站',
+        boxAllocateSn: '1341231',
+        deliveryName: '李丽',
+        totalBox: 423,
         status: 1,
-        totalBox: 158
+        stationName: '浙大站',
+        outTime: 1521429202000,
     },
     {
-        boxAllocateSn: '3213213123123123',
-        devliverName: '李鬼',
-        outTime: '2018-03-19 10:59:44',
-        stationName: '北斗门换电站',
+        boxAllocateSn: '112312',
+        deliveryName: '测试1',
+        totalBox: 42,
         status: 1,
-        totalBox: 444
-    },
-    {
-        boxAllocateSn: '20180313213526542',
-        devliverName: '宋江',
-        outTime: '2018-03-19 10:59:44',
-        stationName: '南斗门换电站',
-        status: 1,
-        totalBox: 523
-    },
-    {
-        boxAllocateSn: '2018031321326542',
-        devliverName: '张xiao chuan',
-        outTime: '2018-03-19 10:59:44',
-        stationName: '西斗门换电站',
-        status: 1,
-        totalBox: 227
-    },
-    {
-        boxAllocateSn: '2018031532131232',
-        devliverName: '',
-        outTime: '2018-03-19 10:59:44',
-        stationName: '东斗门换电站',
-        status: 1,
-        totalBox: 158
-    },
-    {
-        boxAllocateSn: '3213213123123123',
-        devliverName: '李鬼',
-        outTime: '2018-03-19 10:59:44',
-        stationName: '北斗门换电站',
-        status: 1,
-        totalBox: 444
-    },
-    {
-        boxAllocateSn: '20180313213526542',
-        devliverName: '宋江',
-        outTime: '2018-03-19 10:59:44',
-        stationName: '南斗门换电站',
-        status: 1,
-        totalBox: 523
-    },
-    {
-        boxAllocateSn: '2018031321326542',
-        devliverName: '张xiao chuan',
-        outTime: '2018-03-19 10:59:44',
-        stationName: '西斗门换电站',
-        status: 1,
-        totalBox: 227
-    },
-    {
-        boxAllocateSn: '2018031532131232',
-        devliverName: '',
-        outTime: '2018-03-19 10:59:44',
-        stationName: '东斗门换电站',
-        status: 1,
-        totalBox: 158
-    },
-    {
-        boxAllocateSn: '3213213123123123',
-        devliverName: '李鬼',
-        outTime: '2018-03-19 10:59:44',
-        stationName: '北斗门换电站',
-        status: 1,
-        totalBox: 444
-    },
-    {
-        boxAllocateSn: '20180313213526542',
-        devliverName: '宋江',
-        outTime: '2018-03-19 10:59:44',
-        stationName: '南斗门换电站',
-        status: 1,
-        totalBox: 523
-    },
-    {
-        boxAllocateSn: '2018031321326542',
-        devliverName: '张xiao chuan',
-        outTime: '2018-03-19 10:59:44',
-        stationName: '西斗门换电站',
-        status: 1,
-        totalBox: 227
-    },
-    {
-        boxAllocateSn: '2018031532131232',
-        devliverName: '',
-        outTime: '2018-03-19 10:59:44',
-        stationName: '东斗门换电站',
-        status: 1,
-        totalBox: 158
-    },
-    {
-        boxAllocateSn: '3213213123123123',
-        devliverName: '李鬼',
-        outTime: '2018-03-19 10:59:44',
-        stationName: '北斗门换电站',
-        status: 1,
-        totalBox: 444
-    },
-    {
-        boxAllocateSn: '20180313213526542',
-        devliverName: '宋江',
-        outTime: '2018-03-19 10:59:44',
-        stationName: '南斗门换电站',
-        status: 1,
-        totalBox: 523
-    },
-    {
-        boxAllocateSn: '2018031321326542',
-        devliverName: '张xiao chuan',
-        outTime: '2018-03-19 10:59:44',
-        stationName: '西斗门换电站',
-        status: 1,
-        totalBox: 227
-    },
-    {
-        boxAllocateSn: '2018031532131232',
-        devliverName: '',
-        outTime: '2018-03-19 10:59:44',
-        stationName: '东斗门换电站',
-        status: 1,
-        totalBox: 158
-    },
-    {
-        boxAllocateSn: '3213213123123123',
-        devliverName: '李鬼',
-        outTime: '2018-03-19 10:59:44',
-        stationName: '北斗门换电站',
-        status: 1,
-        totalBox: 444
-    },
-    {
-        boxAllocateSn: '20180313213526542',
-        devliverName: '宋江',
-        outTime: '2018-03-19 10:59:44',
-        stationName: '南斗门换电站',
-        status: 1,
-        totalBox: 523
-    },
-    {
-        boxAllocateSn: '2018031321326542',
-        devliverName: '张xiao chuan',
-        outTime: '2018-03-19 10:59:44',
-        stationName: '西斗门换电站',
-        status: 1,
-        totalBox: 227
-    },
-    {
-        boxAllocateSn: '2018031532131232',
-        devliverName: '',
-        outTime: '2018-03-19 10:59:44',
-        stationName: '东斗门换电站',
-        status: 1,
-        totalBox: 158
-    },
-    {
-        boxAllocateSn: '3213213123123123',
-        devliverName: '李鬼',
-        outTime: '2018-03-19 10:59:44',
-        stationName: '北斗门换电站',
-        status: 1,
-        totalBox: 444
-    },
-    {
-        boxAllocateSn: '20180313213526542',
-        devliverName: '宋江',
-        outTime: '2018-03-19 10:59:44',
-        stationName: '南斗门换电站',
-        status: 1,
-        totalBox: 523
-    },
-    {
-        boxAllocateSn: '2018031321326542',
-        devliverName: '张xiao chuan',
-        outTime: '2018-03-19 10:59:44',
-        stationName: '西斗门换电站',
-        status: 1,
-        totalBox: 227
-    },
-    {
-        boxAllocateSn: '2018031532131232',
-        devliverName: '',
-        outTime: '2018-03-19 10:59:44',
-        stationName: '东斗门换电站',
-        status: 1,
-        totalBox: 158
-    },
-    {
-        boxAllocateSn: '3213213123123123',
-        devliverName: '李鬼',
-        outTime: '2018-03-19 10:59:44',
-        stationName: '北斗门换电站',
-        status: 1,
-        totalBox: 444
-    },
-    {
-        boxAllocateSn: '20180313213526542',
-        devliverName: '宋江',
-        outTime: '2018-03-19 10:59:44',
-        stationName: '南斗门换电站',
-        status: 1,
-        totalBox: 523
-    },
-    {
-        boxAllocateSn: '2018031321326542',
-        devliverName: '张xiao chuan',
-        outTime: '2018-03-19 10:59:44',
-        stationName: '西斗门换电站',
-        status: 1,
-        totalBox: 227
-    },
-    {
-        boxAllocateSn: '2018031532131232',
-        devliverName: '',
-        outTime: '2018-03-19 10:59:44',
-        stationName: '东斗门换电站',
-        status: 1,
-        totalBox: 158
-    },
-    {
-        boxAllocateSn: '3213213123123123',
-        devliverName: '李鬼',
-        outTime: '2018-03-19 10:59:44',
-        stationName: '北斗门换电站',
-        status: 1,
-        totalBox: 444
-    },
-    {
-        boxAllocateSn: '20180313213526542',
-        devliverName: '宋江',
-        outTime: '2018-03-19 10:59:44',
-        stationName: '南斗门换电站',
-        status: 1,
-        totalBox: 523
-    },
-    {
-        boxAllocateSn: '2018031321326542',
-        devliverName: '张xiao chuan',
-        outTime: '2018-03-19 10:59:44',
-        stationName: '西斗门换电站',
-        status: 1,
-        totalBox: 227
-    },
-    {
-        boxAllocateSn: '2018031532131232',
-        devliverName: '',
-        outTime: '2018-03-19 10:59:44',
-        stationName: '东斗门换电站',
-        status: 1,
-        totalBox: 158
+        stationName: '黄龙站',
+        outTime: 1521072000000,
     },
 ];
 
-module.exports = alllocateList;
+const allocateHeader = {
+    addTime: 1521378009000,
+    allocateDesc: null,
+    boxAllocateSn: "00020022018031948968",
+    deliveryName: "王五",
+    deliveryTel: "18768101233",
+    operatorId: "",
+    operatorName: "",
+    outTime: 1515254400000,
+    stationId: "2",
+    stationName: "福地元站",
+    status: 1,
+    totalBox: 55,
+    updateTime: 1521789588000,
+    warehousingSn: null,
+}
+
+const boxDetail = {
+    boxSn: '1234567',
+    goodsId: [123, 345, 567],
+    goodsBarCode: [156654352341, 124312412321, 321312446423],
+    goodsName: ['为她柠檬茶', '情爱酸奶', '父亲牛肉棒'],
+    boxExpirationDate: '2019-01-21',
+}
+
+const boxDetailList = [
+    {
+        boxSn: '1234567',
+        goodsId: [123, 345, 567],
+        goodsBarCode: [156654352341, 124312412321, 321312446423],
+        goodsName: ['为她柠檬茶', '情爱酸奶', '父亲牛肉棒'],
+        boxExpirationDate: '2019-01-21',
+    },
+    {
+        boxSn: '1234567',
+        goodsId: [123, 345, 567],
+        goodsBarCode: [156654352341, 124312412321, 321312446423],
+        goodsName: ['为她柠檬茶', '情爱酸奶', '父亲牛肉棒'],
+        boxExpirationDate: '2019-01-21',
+    },
+    {
+        boxSn: '1234567',
+        goodsId: [123, 345, 567],
+        goodsBarCode: [156654352341, 124312412321, 321312446423],
+        goodsName: ['为她柠檬茶', '情爱酸奶', '父亲牛肉棒'],
+        boxExpirationDate: '2019-01-21',
+    },
+    {
+        boxSn: '1234567',
+        goodsId: [123, 345, 567],
+        goodsBarCode: [156654352341, 124312412321, 321312446423],
+        goodsName: ['为她柠檬茶', '情爱酸奶', '父亲牛肉棒'],
+        boxExpirationDate: '2019-01-21',
+    },
+]
+
+const stationList = [
+    {
+        id: 1,
+        name: '西斗门路站点',
+        stationId: 1
+    },
+    {
+        id: 2,
+        name: '东斗门路站点',
+        stationId: 2
+    },
+    {
+        id: 3,
+        name: '南斗门路站点',
+        stationId: 3
+    },
+    {
+        id: 4,
+        name: '北斗门路站点',
+        stationId: 4
+    },
+    {
+        id: 5,
+        name: '中斗门路站点',
+        stationId: 5
+    },
+];
+
+module.exports = {
+    allocateList: allocateDTOList,
+    boxDetail: boxDetail,
+    allocateHeader: allocateHeader,
+    boxDetailList: boxDetailList,
+    stationList: stationList
+}
+
--- routes/scm.js
+++ routes/scm.js
@@ -4,27 +4,108 @@ const router = express.Router()
 const log4js = require('log4js')
 const logger = log4js.getLogger()
 
-const allocateList = require('../mooc/allocate')
+const { allocateList, boxDetail, allocateHeader, boxDetailList, stationList } = require('../mooc/allocate')
 
 const allAllocate = {
     data: {
         totalCount: 6,
         totalPage: 3,
         allocateDTOList: allocateList,
-        err_msg: 'success',
-        error_code: 0,
     },
+    err_msg: 'success',
+    error_code: 0,
 }
 
 /* GET users listing. */
-router.all('/allocate/findAll', function(req, res, next) {
+router.all('/allocate/search', function(req, res, next) {
     setImmediate(function() {
         logger.debug('params', req.params)
         logger.debug('query', req.query)
         logger.debug('body', req.body)
-        
+        res.json(allAllocate)
     })
-    res.json(allAllocate)
+})
+
+router.all('/allocate/boxDetail', function(req,res, next) {
+    setImmediate(() =>{
+        logger.debug('boxDetail -- query', req.query);
+        
+        const result =  {
+            data: boxDetail,
+            err_msg: 'success',
+            error_code: 0,
+        }
+        res.json(result);
+    });
+});
+
+router.all('/allocate/openAllocateInfo/:boxAllocateSn', function(req, res, next) {
+    logger.debug('openAllocateInfo -- params', req.params);
+    setImmediate(() => {
+        const result = {
+            data: {
+                allocateInfoDTO: allocateHeader
+            },
+            err_msg: 'success',
+            error_code: 0,
+        }
+        res.json(result);
+    });
+});
+
+router.all('/allocate/openAllocate/:boxAllocateSn', function(req, res, next) {
+    logger.debug('openAllocate -- params', req.params);
+    logger.debug('openAllocate -- query', req.query);
+    setImmediate(() => {
+        const result = {
+            data: {
+                totalCount: 6,
+                totalPage: 3,
+                boxList: boxDetailList,
+            },
+            err_msg: 'success',
+            error_code: 0,
+        }
+        res.json(result);
+    });
+});
+
+router.all('/allocate/newAllocate', function(req, res, next) {
+    setImmediate(() => {
+        const allocateInfo = req.body;
+        logger.debug('newAllocate -- body', allocateInfo);
+        const result = {
+            data: 'FB20180306223674',
+            err_msg: 'success',
+            error_code: 0,
+        }
+        res.json(result);
+    });
+});
+
+router.all('/allocate/insertBox', function(req, res, next) {
+    setImmediate(() => {
+        const boxlist = req.body;
+        logger.debug('insertBox -- body', boxlist);
+        const result = {
+            data: '',
+            err_msg: 'success',
+            error_code: 0,
+        }
+        res.json(result);
+    });
+});
+
+router.all('/scm-web/station/list', function(req, res, next) {
+    setImmediate(() => {
+        logger.debug('tationlist -- body');
+        const result = {
+            data: stationList,
+            err_msg: 'success',
+            error_code: 0,
+        }
+        res.json(result)
+    })
 })
 
 module.exports = router
